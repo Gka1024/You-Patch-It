13ce7b794fe788ce88595bf1f7d64ecb16024207
Gka1024 <ththtlddkswn@gmail.com>
2026-09-08 23:34:15 +0900

    [Add] Support Characters

---
+++ Assets/Scripts/Character/Skill/CharacterSkill_601_Bard.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Character/Skill/Bard")]
+public class CharacterSkill_Bard : CharacterSkill
+{
+    public override void Execute(BattleCharacter self, BattleCharacter enemy, float coefficient)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Character/Skill/CharacterSkill_602_Miko.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Character/Skill/Miko")]
+public class CharacterSkill_Miko : CharacterSkill
+{
+    public override void Execute(BattleCharacter self, BattleCharacter enemy, float coefficient)
+    {
+        
+    }
+}
